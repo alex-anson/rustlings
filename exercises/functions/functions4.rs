a6d270c395a7beea606cd0c819d9351f16a7482d
Alex Anson <1alex.anson@gmail.com>
2023-04-29 09:52:01 -0400

    loops.. examples from the book

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -6,10 +6,57 @@
 // (Don't worry about the function bodies themselves, we're only interested
 // in the signatures for now. If anything, this is a good way to peek ahead
 // to future exercises!)
-
+// I AM NOT DONE
 fn main() {
     let original_price: i32 = 112;
     println!("Your sale price is {}", sale_price(original_price));
+
+    println!();
+    another_fn();
+    println!();
+    another_another()
+}
+
+fn another_fn() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            // this value will be stored in `result`
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+// multiple loops? (optionally) label them.
+fn another_another() {
+    let mut count = 0;
+    // loop labels must begin with a single quote
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                // this break statement applies to the unnamed loop, because it's
+                // the innermost loop
+                break;
+            }
+            if count == 2 {
+                // this break statement applies to the named loop, because we said so
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
 
 // so this is how you type a return value.
