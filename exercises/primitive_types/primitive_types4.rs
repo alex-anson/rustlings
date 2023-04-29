e997af94cc72823ef7dfa34e5615d4b37b174047
Alex Anson <1alex.anson@gmail.com>
2023-04-29 11:24:21 -0400

    primitive types exercises done.. time for the book! :tada:

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,14 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // tried &a[1..3] first, it's obviously not inclusive
+    // let nice_slice = &a[1..4];
+    // COOOOOL
+    let nice_slice = &a[1..a.len() - 1];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
