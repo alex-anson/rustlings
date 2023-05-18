0d8f9002291cba2f1a0c4b0603b3e9f3a3ff53e7
Alex Anson <1alex.anson@gmail.com>
2023-05-18 18:45:41 -0400

    finish struct notes

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,6 +2,14 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
+/* SUMMARY (The Book)
+Structs let you create custom types that are meaningful for your domain. By
+using structs, you can keep associated pieces of data connected to each other
+and name each piece to make your code clear. In impl blocks, you can define
+functions that are associated with your type, and methods are a kind of associated
+function that let you specify the behavior that instances of your structs have.
+*/
+
 struct ColorClassicStruct {
     red: i32,
     green: i32,
@@ -19,6 +27,8 @@ struct Rectangle {
     height: u32,
 }
 
+// you're allowed to have multiple `impl` blocks for one struct. useful for
+// generic types and traits.
 impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
@@ -35,8 +45,25 @@ impl Rectangle {
     }
 
     // ASSOCIATED FUNCTION
+    // doesn't need an instance of the Rectangle type to work with.
+    // "Associated functions that aren’t methods are often used for constructors
+    // that will return a new instance of the struct."
     fn square(size: u32) -> Self {
         Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    // associated functions that return a new instance of the struct are often
+    // called "new" - but "new" isn't a special name / built into the language
+    // ^ i'm guessing this is convention
+    fn new(width: u32, height: u32) -> Self {
+        Self { width, height }
+    }
+
+    fn can_i_do_this(size: u32) -> Rectangle {
+        Rectangle {
             width: size,
             height: size,
         }
@@ -71,9 +98,17 @@ fn main() {
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     println!();
-}
 
-// I AM NOT DONE
+    let mut square1 = Rectangle::square(3);
+    square1.width = 12;
+    println!("square1's new width is {}", square1.width);
+
+    let new_rect = Rectangle::new(40, 30);
+    println!("the area of new_rect is {}", new_rect.area());
+
+    let yeah = Rectangle::can_i_do_this(32);
+    println!("yeah's height {}", yeah.height);
+}
 
 #[cfg(test)]
 mod tests {
@@ -115,7 +150,7 @@ mod tests {
     #[test]
     fn easy_fail() {
         main();
-        assert_eq!(11, 12)
+        assert_eq!(12, 12)
     }
 
     // #[test]
