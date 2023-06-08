3ce16ffcee4ce49595ac7590d5585d67cadf1582
Alex Anson <1alex.anson@gmail.com>
2023-06-08 16:32:23 -0400

    done with enums

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -27,4 +27,31 @@ fn main() {
     for message in &messages {
         message.call();
     }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        // ^ adding a match guard to a pattern
+        // downside - the compiler won't try to do exhaustiveness checking
+        // 🤨 uhm i feel like this is incorrect ^ .. see below
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+
+    let mut experiment: Option<u32> = None;
+    experiment = Some(12);
+
+    match experiment {
+        Some(a) if a % 2 == 0 => not_nullishh(a),
+        Some(a) if a % 2 != 0 => not_nullishh(a),
+        // missing match arm: `Some(_)` not covered
+        // ^ that's what i'd call exhaustiveness checking...
+        Some(a) => not_nullishh(a),
+        None => println!("it's the closest thing to null that Rust has"),
+    }
+}
+
+fn not_nullishh(x: u32) {
+    println!("x has a value: {x}")
 }
--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,8 +2,6 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
     ChangeColor(u8, u8, u8),
@@ -45,7 +43,7 @@ impl State {
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
         match message {
             Message::ChangeColor(a, b, c) => self.change_color((a, b, c)),
-            Message::Echo(d) => self.echo(d),
+            Message::Echo(d) => self.echo(d + " ... what an adventure"),
             Message::Move(e) => self.move_position(e),
             Message::Quit => self.quit(),
         }
