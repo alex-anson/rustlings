7a9985a939642dfd12632de41042e2d01a25647f
Alex Anson <1alex.anson@gmail.com>
2023-06-08 17:31:26 -0400

    string exercises

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,26 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut a = String::from(input);
+    a.push_str(" world!");
+    // interesting that i need to return "a" in a separate line... it's been a while;
+    // i guess i've forgotten some of the rules...
+    a
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let a = String::from(input);
+    // .replace creates & returns a new string, that's why there's no need for "a"
+    // to be mutable, and why i don't need to return "a" on a separate line.
+    a.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -37,7 +42,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
