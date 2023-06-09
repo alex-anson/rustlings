1d1a1528c8bb4b4c5dd5fa1e379771152f441b89
Alex Anson <1alex.anson@gmail.com>
2023-06-08 20:42:32 -0400

    notes on strings

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -5,8 +5,55 @@
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
+
+    whhht();
+    println!();
+    example_two();
 }
 
 fn current_favorite_color() -> String {
     String::from("teal")
+}
+
+fn whhht() {
+    let hello = "Здравствуйте";
+    // let one = &hello[0];
+    let one = &hello.bytes().nth(0);
+
+    println!("aayyooo {}", hello.len()); // prints 24 --
+                                         // even though it's 12 characters long, it
+                                         // takes 24 bytes to encode it in UTF-8.
+
+    println!("result... {:?}", one);
+    // ^ prints result... Some(208)
+
+    // (when encoded in UTF-8, the first byte is 208.)
+}
+
+/*
+- strings are implemented as a collection of bytes, plus some methods to provide
+useful functionality when those bytes are interpreted as text.
+
+- since the String type is implemented with vectors under the hood, can grow/shrink
+/manipulate the contents
+*/
+
+fn example_two() {
+    let hello = "hello";
+    // let one = &hello[0];
+    // ^ if this didn't make the compiler throw an error, "one" would contain the
+    // value 104. since that's probably not what humans would want when they're
+    // asking for the first letter of a string, Rust prevents the code from compiling
+    let one = &hello.bytes().nth(0);
+
+    println!("result... {:?}", one);
+    // ^ prints result... Some(104)
+
+    match one {
+        Some(x) => println!("result... {:?}", x),
+        // ^ prints result... 104
+        None => println!("nope"),
+    }
+
+    // (when encoded in UTF-8, the first byte is 104. hence the result)
 }
--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -9,8 +9,69 @@ fn main() {
     } else {
         println!("That is not a color word I know.");
     }
+    println!();
+    println!();
+
+    // Hindi word, written in the Devanagari script
+    let x = "नमस्ते";
+
+    vector_values(x);
+    println!();
+    unicode_scalar_values(x);
+    println!();
+    with_ranges_be_careful(x);
+    println!();
+    iterating_over_strings(x);
 }
 
 fn is_a_color_word(attempt: &str) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
+}
+
+fn vector_values(word: &str) {
+    let byte_vec = word.as_bytes();
+    println!("{:?}", byte_vec);
+    // u8 vector values.
+    println!("{} bytes long", byte_vec.len()) // 18
+}
+
+fn unicode_scalar_values(word: &str) {
+    let char_type = word.chars();
+    println!("{:?}", char_type);
+    // ^ result looks a little different than in The Book; they note that the fourth
+    // and sixth values are "diacritics" that don't make sense on their own -- and
+    // those are the ones that look different... ex: '\u{94d}'
+
+    /*
+    It's important to remember that char represents a Unicode Scalar Value, and
+    might not match your idea of what a 'character' is. Iteration over grapheme
+    clusters may be what you actually want. This functionality is not provided
+    by Rust's standard library, check crates.io instead.
+    */
+    let hm = word.chars();
+    // ^ comment is from .chars() hover. grapheme clusters is what i was looking
+    // for. they're the closest thing to what we would call letters.
+}
+
+fn with_ranges_be_careful(word: &str) {
+    // let hello = &word[0];
+    // ^ not possible. can do ranges:
+    let hello = &word[0..6];
+    println!("{:?}", hello)
+    // be careful using ranges to create string slices. doing so can
+    // crash your program
+}
+
+fn iterating_over_strings(word: &str) {
+    // unicode scalar values
+    for c in word.chars() {
+        println!("{c}")
+        // interestingly, the fourth and sixth chars look like they look in The
+        // Book when iterating over them here.
+    }
+
+    // raw bytes
+    for b in word.bytes() {
+        println!("{b}")
+    }
 }
