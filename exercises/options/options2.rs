89033b457ee700b69b4e395262371f7a9f50a839
Alex Anson <1alex.anson@gmail.com>
2023-07-01 14:05:16 -0400

    options exercises. gonna need more practice w this...

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -1,8 +1,6 @@
 // options2.rs
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -11,8 +9,10 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-       if let word = Some(optional_target) {
-            assert_eq!(word, target);
+        if let word = optional_target {
+            assert_eq!(word, Some(target));
+            // definitely feels like i've done this the wrong way. it's hard to
+            // discern what exactly they're looking for in this exercise...
         }
     }
 
@@ -26,9 +26,11 @@ mod tests {
 
         // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
         // You can stack `Option<T>`'s into while let and if let
-        integer = optional_integers.pop() {
-            assert_eq!(integer, range);
+        while let Some(integer) = optional_integers.pop() {
+            assert_eq!(integer, Some(range));
             range -= 1;
         }
+        // yeah i'm confused. gonna need to spend some time on this when i start
+        // actually going through The Book
     }
 }
