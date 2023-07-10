3ae1026f5dc8d7720910dcbfd67f074ecb4ebc81
Alex Anson <1alex.anson@gmail.com>
2023-07-10 19:18:08 -0400

    quiz 3 - start over

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -15,13 +15,6 @@
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
-// use std::fmt::Display;
-
-pub struct Alph {
-    pub grade: String,
-    pub student_name: String,
-    pub student_age: u8,
-}
 
 pub struct ReportCard {
     pub grade: f32,
@@ -29,21 +22,8 @@ pub struct ReportCard {
     pub student_age: u8,
 }
 
-pub trait Print {
-    fn print(&self) -> String;
-}
-
-impl Print for ReportCard {
-    fn print(&self) -> String {
-        format!(
-            "{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade
-        )
-    }
-}
-
-impl Print for Alph {
-    fn print(&self) -> String {
+impl ReportCard {
+    pub fn print(&self) -> String {
         format!(
             "{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade
@@ -71,8 +51,8 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = Alph {
-            grade: "A+".to_string(),
+        let report_card = ReportCard {
+            grade: 2.1,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
