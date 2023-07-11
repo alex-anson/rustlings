4e8e3fa9e97c5ba6a37c3b0722dc29739d04ae9e
Alex Anson <1alex.anson@gmail.com>
2023-07-10 20:10:40 -0400

    tests exercises + some stuff from The Book

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -3,12 +3,15 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(12, 12);
+    }
+
+    #[test]
+    fn you_can_assert_not_eq() {
+        assert_ne!(12, 11)
     }
 }
--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -4,23 +4,29 @@
 // we expect to get when we call `is_even(5)`.
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
 
+/*
+"The tests module is a regular module that follows the usual visibility rules
+we covered in Chapter 7 in the “Paths for Referring to an Item in the Module
+Tree” section. => https://doc.rust-lang.org/book/ch07-03-paths-for-referring-to-an-item-in-the-module-tree.html
+*/
 #[cfg(test)]
 mod tests {
     use super::*;
+    // ^ brings everything from the outer scope into the scope of this
+    // inner module.
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let result = is_even(12);
+        assert!(result);
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(!is_even(5));
     }
 }
